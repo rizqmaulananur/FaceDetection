afcdc825ef2306b87effd4f14fe253ab0f7d2bc4
Rizky Maulana <rizkymaulanaID@outlook.com>
2021-08-16 15:05:34 +0700

    init

--- DeteksiWajah.py
+++ DeteksiWajah.py
@@ -10,7 +10,7 @@ while True:
     faces = faceDetector.detectMultiScale(Grey, 1.3, 5) #frame, scaleFactor,
     for(x, y, w, h) in faces:
         frame = cv2.rectangle(frame, (x, y), (x+y, y+h),(0,255,255), 2)
-    cv2.imshow('webcamku', frame)
+    cv2.imshow('Camera', frame)
     #cv2.imshow('webcamku 2', Grey)
     k = cv2.waitKey(1) & 0xFF
     if k == 27 or k == ord('q'):
