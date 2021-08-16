c02c3bf82989b1b7eed52a3cce61a08bc1509370
Rizky Maulana <rizkymaulanaID@outlook.com>
2021-08-16 15:03:59 +0700

    Tes

--- webcam.py
+++ webcam.py
@@ -2,9 +2,9 @@ import cv2
 cam = cv2.VideoCapture(0)
 while True:
     retV, frame = cam.read()
-    abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    Grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('webcamku', frame)
-    cv2.imshow('webcamku 2', abuAbu)
+    cv2.imshow('webcamku 2', Grey)
     k = cv2.waitKey(1) & 0xFF
     if k == 27 or k == ord('q'):
         break
